8600b97e7f8993c75d88fdabccefde9fcdaa9c3f
Trevor Seeger <tadam.sgr@gmail.com>
2024-02-29 15:23:00 -0700

    Conversion to Git Friendly format

--- Linear Regression Functions.R
+++ Linear Regression Functions.R
@@ -20,22 +20,6 @@
 
 # These functions can be used as a companion to the "Regressions Functions.Rmd" file or as standalone functions
 
-##### Visualise data #####
-
-# For an individual relationship to review
-plot.dat = function(dat, #The dataset, must be a matrix of at least two columns
-                    y, #Dependent Variable
-                    x1 #Another variable to compare to Y
-) {
-  plot(dat[[y]],dat[[x1]],
-       #xlim=c(3,5),ylim=c(8,26),pch=19,
-       xlab=x1,ylab=y,las=1)
-  mtext("y",side=2,las=1,line=2)
-}
-
-# Can also use pairs(dat) for all veriables at once
-
-
 
 ##### Testing Assumptions of Linear Regressions ####
 
